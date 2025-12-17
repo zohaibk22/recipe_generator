ca20b7927b5c43850c20de9b262054bfa51c5cb3
Zohaib Khan <zohaibk1111@gmail.com>
2025-12-17 01:43:25 -0500

    initial commit

---
+++ src/main.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+
+API_KEY = os.environ.get("OPENAI_API_KEY")
+def main():
+
+    llm = OpenAI(openai_api_key=API_KEY, temperature=0.9)
+
+    # Define prompt template
+    prompt_template = PromptTemplate(
+        template= "Give me am example of a meal that could be made using the following ingredients: {ingredients}",
+        input_variables=["ingredients"]
+    )
+
+    # Test the prompt template
+    template = prompt_template.format(ingredients="chicken, rice, broccoli")
+
+    print(template)
+
+    # Create LLM Chain
+    meal_chain = LLMChain(llm=llm, prompt=prompt_template)
+
+    st.title("Recipe Generator")
+    user_prompt = st.text_input("Enter ingredients (comma separated):")
+
+    if st.button("Generate Recipe") and user_prompt:
+        output = llm(prompt_template.format(ingredients=user_prompt))
+        st.write(output )
+    
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
